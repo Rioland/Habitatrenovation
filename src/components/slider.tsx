dff14ca21c492ed56519ce1b38e6122d5376f14c
cleaques_sys1 <adedamolarioland2222@gmail.com>
2024-10-14 07:26:25 -0700

    first commit

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -11,14 +11,14 @@ const Carousel = () => {
   <div className="carousel-inner">
     <div className="carousel-item active">
    
-      <img src="/images/slider.jpg" className="d-block w-100" alt="..." />
+      <img src="/images/banner1.jpeg" height={600} className="d-block w-100" alt="..." />
     </div>
-    {/* <div className="carousel-item ">
-      <img src="https://cdn11.bigcommerce.com/s-sryqni/images/stencil/original/carousel/449/BuyMore-2000x538.jpg?c=2" className="d-block w-100" alt="..." />
-    </div> */}
-    {/* <div className="carousel-item ">
-      <img src="https://cdn11.bigcommerce.com/s-sryqni/images/stencil/original/carousel/463/Home-Page-Banner-2000X538.jpg?c=2" className="d-block w-100" alt="..." />
-    </div> */}
+    <div className="carousel-item active ">
+      <img src="/images/banner2.jpeg" height={600} className="d-block w-100" alt="..." />
+    </div>
+    <div className="carousel-item active">
+      <img src="/images/banner3.jpeg" height={600} className="d-block w-100" alt="..." />
+    </div>
     {/* <div className="carousel-item active">
       <img src="https://cdn11.bigcommerce.com/s-sryqni/images/stencil/original/carousel/460/getyourdoors-2000x538__87999.jpg?c=2" className="d-block w-100" alt="..." />
     </div> */}
